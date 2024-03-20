628c0a8439b9df6c8d2b65c724bade8c6067512d
Chunfield <1252742605@qq.com>
2024-03-20 22:29:51 +0800

    feat:增加了移动长图表的功能

--- src/components/Chart/index.vue
+++ src/components/Chart/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="green">
         <div class="charts" ref="chart"></div>
-        <button @click="handleLeftButtonClick" class="ar">←</button>
+        <button @click="zoomLeft" class="ar1">&lt;</button>
+        <button @click="zoomRight" class="ar2">&gt;</button>
     </div>
 </template>
 
@@ -11,6 +12,10 @@ export default{
     data(){
         return{
             chart:'',
+            dataZoom: {  
+                start: 10,  
+                end: 70  
+                    },
             temdata:[this.nextWeek[0]?.tem,this.nextWeek[1]?.tem,this.nextWeek[2]?.tem,this.nextWeek[3]?.tem,this.nextWeek[4]?.tem,this.nextWeek[5]?.tem,this.nextWeek[6]?.tem,],
         }
     },
@@ -27,7 +32,8 @@ export default{
                     text:"未来七天气温预报",
                     textStyle: {   
                     color: '#fff'   
-                                }  
+                                },
+                    top:'10px'  
                 },
                 xAxis:{
                     type: 'category',
@@ -59,24 +65,25 @@ export default{
                         }
                     }  
                 },
-    //             dataZoom: [  
-    //     {  
-    //         type: 'slider',   
-    //         start: 10,  
-    //         end: 60 
-    //     },  
-    //     {  
-    //         type: 'inside', 
-    //         start: 10,  
-    //         end: 60  
-    //     }  
-    // ],
+              
                 tooltip: {  
                     trigger: 'item', 
                     axisPointer: {  
                     type: 'line'  
                                 },
-                        },  
+                    backgroundColor:'rgba(64, 60, 60, 0.1)',
+                    textStyle:{
+                        color:'#fff'
+                              },       
+                    },
+                       
+                        dataZoom: [  
+                        {
+                        show:false,  
+                        start: this.dataZoom.start,  
+                        end: this.dataZoom.end,  
+                        }  
+                                    ], 
     
                 series: [{  
                     name: '温度',  
@@ -101,23 +108,67 @@ export default{
                     data: [this.temdata[0],this.temdata[1],this.temdata[2],this.temdata[3],this.temdata[4],this.temdata[5],this.temdata[6],]  
                         }]  
             }
+            
             this.chart.setOption(option);
-       }
+       },
+       zoomLeft() {  
+      
+      if (this.dataZoom.start > 0) {  
+        this.dataZoom.start -= 10;  
+        this.dataZoom.end -= 10;  
+        this.updateChart();  
+      }
+    },
+
+      zoomRight() {    
+      if (this.dataZoom.end < 100) {  
+        this.dataZoom.start += 10;  
+        this.dataZoom.end += 10;  
+        this.updateChart();  
+      }  
+    },
+
+    updateChart() {  
+      this.chart.setOption({  
+        dataZoom: [{  
+          start: this.dataZoom.start,  
+          end: this.dataZoom.end  
+            }]  
+        });  
+        },    
     }
 }
+
 
 </script>
 <style>
 .charts{
     height:250px;
-    width: 100%;
+    width: 85%;
+    margin: 0 auto;
 }
 .green{
-    background-color: green;
+    position: relative;
+    background-color: rgba(240, 236, 236, 0.056);
+    border-radius: 10px;
 }
-.ar{
+.ar1{
     position: absolute;
-    height: 50px;
-    width: 50px;
+    top: 50%;
+    transform: translateY(-50%);
+    background-color: transparent;
+    border: none;
+    font-size: 32px;
+    color: aliceblue;
+}
+.ar2{
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    background-color: transparent;
+    border: none;
+    font-size: 32px;
+    color: aliceblue;
 }
 </style>
